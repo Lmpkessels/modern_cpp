264e84de81c68f30a6747bbf689697db46fdc68b
lkessels <l@lmpkessels.com>
2026-07-25 18:09:35 +0200

    add: algorithm to double or tripple num based on user input"

--- code/double_or_tripple.cpp
+++ code/double_or_tripple.cpp
@@ -2,29 +2,52 @@
 
 #define ANSWER_BUFF 7 // Answer for double or tripple question
 
+// Double num
 int double_int(int num) 
 {
     return num * 2;
 }
 
+// Tripple num
 int tripple_int(int num)
 {
     return num * 3;
 }
 
-int choose()
+// Transform all characters to lower case
+std::string to_lower(std::string str)
 {
-    std::cout << "Please provide a number: ";
-    int received_num {}
+    for (char& c : str)
+    {
+        c = std::tolower(c);
+    }
 
-    std::cout << "Do you want to double or tripple the number?";
-    char answer[ANSWER_BUFF];
+    return str;
+}
+
+// Choose to double or tripple the number of choice
+void choose()
+{
+    int received_num {};
+
+    std::cout << "Please provide a number: ";
+    std::cin >> received_num;
+
+    std::cout << "Do you want to double or tripple the number? ";
+
+    std::string answer {};
     std::cin >> answer;
 
-    if ((char)tolower(answer) == "double") {
-        double_int(received_num);
-    } else if (char(tolower(answer) == "tripple")) {
-        tripple_int(received_num);
+    answer = to_lower(answer);
+
+    // After receiving the number and answer on double or tripple make the 
+    // decision
+    if (answer == "double") {
+        std::cout << double_int(received_num) << '\n';
+    } else if (answer == "tripple") {
+        std::cout << tripple_int(received_num) << '\n';
+    } else {
+        std::cout << "Invalid option\n";
     }
 }
 
