92300e5ce8202c56466aeb55dafd74f747814f15
lkessels <l@lmpkessels.com>
2026-07-28 13:29:09 +0200

    add: calculater for square with the use of header files

--- code/header_files/main.cpp
+++ code/header_files/main.cpp
@@ -1,7 +1,14 @@
 #include "add.hpp"
+#include "square.hpp"
 #include <iostream>
 
 int main(void)
 {
-    question();
+    //question();
+
+    std::cout << "Please provide a number of which you want the square: ";
+    int input{};
+    std::cin >> input;
+
+    std::cout << "The square of " << input <<  " is " << square(input) << ".\n";
 }
---
+++ code/header_files/square.hpp
@@ -0,0 +1,7 @@
+#ifndef SQUARE_HPP
+#define SQUARE_HPP
+
+int get_square_sides();
+int square(int side_length);
+
+#endif
